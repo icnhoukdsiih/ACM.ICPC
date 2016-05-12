f1f40460c177b7430b8ea1268730c1d341c3af86
Nguyen Anh Tuan <tnguyenanh97@gmail.com>
2016-05-12 20:23:46 +0700

    Update QMAX.cpp

--- QMAX.cpp
+++ QMAX.cpp
@@ -1,5 +1,5 @@
 // PREPARING ACM.ICPC 2018
-// NKLINEUP - CODED BY ICNHOUKDSIIH
+// QMAX - CODED BY ICNHOUKDSIIH
 // CONTACT ME: https://icnhoukdsiih.blogspot.com/
 //--------------------------------------------------
 
